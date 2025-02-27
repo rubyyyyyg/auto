3fdc334523d9b108df72f08b889c13698c662dc1
Ruby Guzman <rubyguzmansoto@gmail.com>
2025-02-26 20:53:17 -0700

    completed

---
+++ data/reviews.js
@@ -0,0 +1,39 @@
+export const reviews = [
+  {
+    "name": "Jake Thompson",
+    "rating": 5,
+    "comment": "These guys know their stuff! They transformed my truck exactly how I envisioned. The quality of work is outstanding. Highly recommend!"
+  },
+  {
+    "name": "Amanda Lee",
+    "rating": 4,
+    "comment": "Great service and a professional team. The custom paint job looks amazing. A slight delay in the delivery, but overall very happy with the results."
+  },
+  {
+    "name": "Rachel Garcia",
+    "rating": 5,
+    "comment": "Incredible craftsmanship! My car has never looked better. They really take the time to make sure everything is perfect. Worth every penny!"
+  },
+
+  {
+    "name": "Sophie Wilson",
+    "rating": 5,
+    "comment": "Top-notch service! The team here is friendly, and they truly know how to bring your vision to life. My car's new wheels look amazing!"
+  },
+  {
+    "name": "Ethan Clark",
+    "rating": 4,
+    "comment": "Had my vehicle lifted and new suspension installed. The work was excellent, but it took a bit longer than expected. Overall, satisfied with the end result."
+  },
+  {
+    "name": "Mia Perez",
+    "rating": 5,
+    "comment": "Fantastic experience! The team installed custom leather seats in my SUV, and they look incredible. Very high-quality work."
+  },
+  
+  {
+    "name": "Olivia Scott",
+    "rating": 4,
+    "comment": "Overall, I'm happy with the results. The custom stereo system they installed sounds amazing, but there was a small hiccup with the timing of the installation."
+  }
+]
---
+++ js/scripts.js
@@ -0,0 +1,46 @@
+import { reviews } from '../data/reviews.js';
+
+const navElement = document.querySelector('#theNav');
+const btnElement = document.querySelector('#theButton');
+
+btnElement.addEventListener('click', () => {
+    btnElement.classList.toggle('open');
+    navElement.classList.toggle('open');
+});
+
+const myTarget = document.querySelector('#cards');
+
+for (let x = 0; x < reviews.length; x++) {
+    const mySection = document.createElement('section')
+
+    const myName = document.createElement('h3')
+    myName.textContent = reviews[x].name
+
+    const myRating = document.createElement('div')
+
+    for (let step = 0; step<reviews[x].rating; step++) {
+       
+        const starR = document.createElement('img')
+        starR.src = "./svgCards/star.svg"
+        myRating.appendChild(starR)
+    }
+
+    const myReview = document.createElement('p')
+    myReview.textContent = reviews[x].comment
+
+   
+
+
+   
+
+   
+   
+
+
+    mySection.appendChild(myName)
+    mySection.appendChild(myRating)
+    mySection.appendChild(myReview);
+    
+
+    myTarget.appendChild(mySection);
+}
